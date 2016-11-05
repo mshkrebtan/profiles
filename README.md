45eed878e8a6df8af2e87d6843bb2942e60cd670
Maksim Shkrebtan <mshkrebtan@gmail.com>
2016-11-05 14:58:47 +0300

    Change the README and refine the scripts

--- README.md
+++ README.md
@@ -1,23 +1,41 @@
 # Profiles
 
-These are my profiles I currently use:
+Here are my profiles for Bash, Vim, Tmux and interactive Python shell.
 
-- `.bash_profile`
-- `.vimrc`
-- `.tmux.conf`
-- `.pythonrc`
-
-# Vim
-
-Vim files:
-
-- `.vim/bundle/tabular/after/plugin/my_tabular_commands.vim` — custom commands for the Tabular plugin
-- `.vim/keymap/russian-jcukenmac.vim` — Russian (Macintosh) keyboard layout
-
-## Usage
-
-Just create a symlink to a profile. For example:
+## Bash
 
 ```
-ln -s profiles/.bash_profile ~/.bash_profile
+bash/
+├── .bash_aliases
+├── .bash_profile
+├── .bash_tweaks
+└── .bashrc
+```
+
+## Vim
+
+```
+vim/
+├── .vim
+│   ├── bundle
+│   │   └── tabular
+│   │       └── after
+│   │           └── plugin
+│   │               └── my_tabular_commands.vim
+│   ├── ftdetect
+│   └── keymap
+│       └── russian-jcukenmac.vim
+└── .vimrc
+```
+
+## Installation
+
+1. Clone this repo. Use a branch suitable for your OS:
+```
+git clone https://github.com/mshkrebtan/profiles.git -b macOS
+```
+
+2. Launch the `install.sh` shell script. It will create all the necessary symlinks in your `home` directory.
+```
+sh ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
 
+# Get the installation script directory path
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 install_vim() {
-    # Install Vundle first
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    # Clone Vundle first
+    git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
 
-    # Install .vimrc
+    # Link .vimrc
     ln -s ${DIR}/vim/.vimrc ${HOME}
 
-    # Now install the plugins
-    vim +PluginInstall +qall
-
-    # Install Russian-jcukenmac keymap
+    # Link Russian-jcukenmac keymap
     mkdir -p ${HOME}/.vim/keymap
-    ln -sf ${DIR}/vim/.vim/keymap/russian-jcukenmac.vim \
+    ln -s ${DIR}/vim/.vim/keymap/russian-jcukenmac.vim \
         ${HOME}/.vim/keymap/
 
-    # Install additional tabular commands
+    # Link additional tabular commands
     mkdir -p ${HOME}/.vim/bundle/tabular/after/plugin/
     ln -s ${DIR}/vim/.vim/bundle/tabular/after/plugin/my_tabular_commands.vim \
         ${HOME}/.vim/bundle/tabular/after/plugin/
 
-    # Install filetype definitions
+    # Link filetype definitions
     ln -s ${DIR}/vim/.vim/ftdetect ${HOME}/.vim/
 }
 
-# Tmux
-ln -s ${DIR}/tmux/.tmux.conf} ${HOME}
+install_tmux() {
+    # Clone Tmux Plugin Manager
+    git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
+
+    # Link .tmux.conf
+    ln -s ${DIR}/tmux/.tmux.conf ${HOME}
+}
 
 # Bash
-ln -s ${DIR}/bash/.bashrc ${HOME}/.bashrc
-ln -s ${DIR}/bash/{.bash_profile,.bash_aliases,.bash_tweaks} ${HOME}
+ln -s ${DIR}/bash/.bashrc ${HOME}
+ln -s ${DIR}/bash/.bash_profile ${HOME}
+ln -s ${DIR}/bash/.bash_aliases ${HOME}
+ln -s ${DIR}/bash/.bash_tweaks ${HOME}
 
-# Python
-ln -s ${DIR}/python/.pythonrc ${HOME}
+# Tmux
+install_tmux
 
 # Vim
 install_vim
+
+# Python
+ln -s ${DIR}/python/.pythonrc ${HOME}
