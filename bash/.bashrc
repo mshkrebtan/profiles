9ef5b3d44105ed95dcf821340aef4080a9fe0fd4
Andrey Volochay <andrey@volochay.info>
2017-12-02 22:13:02 +0300

    Make install script idempotent
    - Rewrite installation script to have the idempotent behavior
    - Rename all resources which names starting with dot

--- bash/.bashrc
+++
@@ -1,7 +0,0 @@
-if [ -f ~/.bash_aliases ]; then
-    . ~/.bash_aliases
-fi
-
-if [ -f ~/.bash_tweaks ]; then
-    . ~/.bash_tweaks
-fi
--- install.sh
+++ install.sh
@@ -1,57 +1,79 @@
 #!/bin/bash
 
 # Get the installation script directory path
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
 
 install_vim() {
+  vundle_path="$HOME/.vim/bundle/Vundle.vim"
+
+  if [ ! -d "$vundle_path" ]; then
     # Clone Vundle first
-    git clone https://github.com/VundleVim/Vundle.vim.git \
-        ${HOME}/.vim/bundle/Vundle.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git $vundle_path
+  else
+    cd $vundle_path
+    git fetch origin && git pull
+  fi
 
-    # Link .vimrc
-    ln -s ${DIR}/vim/.vimrc ${HOME}
+  # Link .vimrc
+  ln -sf $DIR/vim/vimrc ~/.vimrc
 
-    # Link Russian-jcukenmac keymap
-    mkdir -p ${HOME}/.vim/keymap
-    ln -s ${DIR}/vim/.vim/keymap/russian-jcukenmac.vim \
-        ${HOME}/.vim/keymap/
+  # Install/Update all plugins
+  vim +PluginUpdate +qall
 
-    # Link additional tabular commands
-    mkdir -p ${HOME}/.vim/bundle/tabular/after/plugin/
-    ln -s ${DIR}/vim/.vim/bundle/tabular/after/plugin/my_tabular_commands.vim \
-        ${HOME}/.vim/bundle/tabular/after/plugin/
+  # Link Russian-jcukenmac keyma
+  mkdir -p ~/.vim/keymap
+  ln -sf $DIR/vim/vim/keymap/russian-jcukenmac.vim \
+    ~/.vim/keymap/
 
-    # Link filetype definitions
-    ln -s ${DIR}/vim/.vim/ftdetect ${HOME}/.vim/
+  # Link additional tabular commands
+  mkdir -p ~/.vim/bundle/tabular/after/plugin/
+  ln -sf $DIR/vim/vim/bundle/tabular/after/plugin/my_tabular_commands.vim \
+    ~/.vim/bundle/tabular/after/plugin/
+
+  # Link filetype definitions
+  ln -sf $DIR/vim/vim/ftdetect ~/.vim/
 }
 
 install_tmux() {
-    # Clone Tmux Plugin Manager
-    git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
+  tmux_plugin_path="$HOME/.tmux/plugins/tpm"
 
-    # Link .tmux.conf
-    ln -s ${DIR}/tmux/.tmux.conf ${HOME}
+  if [ ! -d "$tmux_plugin_path" ]; then
+    # Clone Tmux Plugin Manager
+    git clone https://github.com/tmux-plugins/tpm $tmux_plugin_path
+  else
+    cd $tmux_plugin_path
+    git fetch origin && git pull
+  fi
+
+  # Link .tmux.conf
+  ln -sf $DIR/tmux/tmux.conf ~/.tmux.conf
 }
 
-# Bash
-ln -s ${DIR}/bash/.bashrc ${HOME}
-ln -s ${DIR}/bash/.bash_profile ${HOME}
-ln -s ${DIR}/bash/.bash_aliases ${HOME}
-ln -s ${DIR}/bash/.bash_tweaks ${HOME}
-
 # Git in Bash
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh \
-    -o ${HOME}/.git-prompt.sh
+curl https://raw.githubusercontent.com\
+/git/git/master/contrib/completion/git-prompt.sh \
+  -o ~/.git-prompt.sh
+
+# Python
+ln -sf $DIR/python/pythonrc ~/.pythonrc
 
 # Base16 Shell
-git clone https://github.com/chriskempson/base16-shell.git \
-    $HOME/.config/base16-shell
+base16_shell_path="$HOME/.config/base16-shell"
+if [ ! -d "$base16_shell_path" ]; then
+  git clone https://github.com/chriskempson/base16-shell.git \
+    $base16_shell_path
+else
+  cd $base16_shell_path
+  git fetch origin && git pull
+fi
+
+# Bash
+ln -sf $DIR/bash/bash_profile ~/.bash_profile
+ln -sf $DIR/bash/bash_aliases ~/.bash_aliases
+ln -sf $DIR/bash/bash_tweaks ~/.bash_tweaks
 
 # Tmux
 install_tmux
 
 # Vim
 install_vim
-
-# Python
-ln -s ${DIR}/python/.pythonrc ${HOME}
