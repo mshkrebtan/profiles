0029d69570e9f9b053e339a48bbbaec3d8b91f74
Maksim Shkrebtan <maksim.shkrebtan@traderepublic.com>
2022-04-03 08:55:03 +0200

    arkenfox: Add user-overrides.js

---
+++ arkenfox/user-overrides.js
@@ -0,0 +1,8 @@
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("media.peerconnection.enabled", true);
+user_pref("permissions.default.shortcuts", 2);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("extensions.pocket.enabled", false);
+user_pref("webgl.disabled", false);
