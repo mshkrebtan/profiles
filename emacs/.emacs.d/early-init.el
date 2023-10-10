e56a192bb2f071acb8468ca3539fb5c84f85944f
Maksim Shkrebtan <3735044+mshkrebtan@users.noreply.github.com>
2023-10-10 11:09:43 +0200

    emacs: Update Emacs config

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -1,18 +1,17 @@
 ;; Defer garbage collection further back in the startup process
 (setq gc-cons-threshold most-positive-fixnum)
 
+;; Tab Bar customisations
+(setq tab-bar-show 1
+      tab-bar-tab-hints t)
 
 ;; GUI modes
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-
-
-;; Frame defaults
-(setq ns-use-proxy-icon nil)
-(setq frame-resize-pixelwise t)
+(tab-bar-mode t)
 
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
-(add-to-list 'default-frame-alist '(font . "JetBrains Mono-13"))
+(add-to-list 'default-frame-alist '(font . "JetBrains Mono-12:antialias=none"))
 (add-to-list 'default-frame-alist '(line-spacing . 0.2))
 
 (add-to-list 'frameset-filter-alist '(ns-transparent-titlebar . :never))
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -19,9 +19,9 @@
  completion-styles '(flex)
  completions-format 'one-column
  confirm-kill-emacs 'yes-or-no-p
+ csv-separators '(";" "	")
  desktop-save 'if-exists
  ediff-split-window-function 'split-window-horizontally
- exec-path (append '("/usr/local/bin/") exec-path)
  flyspell-issue-message-flag nil
  org-agenda-files '("~/org"
                     "~/work")
@@ -39,7 +39,7 @@
  org-startup-indented t
  read-buffer-completion-ignore-case t
  read-file-name-completion-ignore-case t
-ring-bell-function 'ignore
+ ring-bell-function 'ignore
  save-interprogram-paste-before-kill t
  vc-follow-symlinks nil
  what-cursor-show-names t)
@@ -63,11 +63,16 @@ ring-bell-function 'ignore
 
 
 ;; Set environment variables
-(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 (setenv "SSH_AUTH_SOCK" (expand-file-name "~/.ssh/ssh-agent"))
+(setenv "LANG" "en_GB.UTF-8")
 
 
 ;; Set key bindings
+(global-set-key "\C-c\C-z." 'browse-url-at-point)
+(global-set-key "\C-c\C-zb" 'browse-url-of-buffer)
+(global-set-key "\C-c\C-zr" 'browse-url-of-region)
+(global-set-key "\C-c\C-zu" 'browse-url)
+(global-set-key "\C-c\C-zv" 'browse-url-of-file)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
@@ -113,19 +118,17 @@ ring-bell-function 'ignore
   (setq ispell-personal-dictionary "~/.hunspell_personal"))
 
 
-;; Packages
-(use-package modus-themes
-  :ensure
-  :init
-  (setq modus-themes-syntax '(faint alt-syntax))
-  (modus-themes-load-themes)
-  :config
-  (modus-themes-load-operandi))
-
 (use-package magit
   :ensure t
   :bind (("C-x g" . magit-status)
          ("C-x M-g" . magit-dispatch)))
+
+(use-package treesit-auto
+  :ensure t
+  :config
+  (setq treesit-auto-install 'prompt)
+  (setq auto-mode-alist (rassq-delete-all 'yaml-ts-mode auto-mode-alist)))
+
 
 (use-package markdown-mode
   :ensure t
@@ -133,7 +136,9 @@ ring-bell-function 'ignore
   :mode (("README\\.md\\'" . gfm-mode))
   :init
   (setq markdown-command "markdown")
-  :hook ((gfm-mode . turn-on-auto-fill)
+  :hook ((markdwon-mode . turn-on-auto-fill)
+         (markdown-mode . display-fill-column-indicator-mode)
+         (gfm-mode . turn-on-auto-fill)
          (gfm-mode . display-fill-column-indicator-mode)))
 
 
@@ -141,19 +146,29 @@ ring-bell-function 'ignore
   :ensure t
   :commands grip-mode
   :init
-  (load-file "~/.emacs.d/grip-mode-sensitive.el")
-  :hook (markdown-mode . grip-mode))
+  (load-file "~/.emacs.d/grip-mode-sensitive.el"))
 
 (use-package terraform-mode
   :ensure t
   :commands terraform-mode
+  :config
+  (setq terraform-format-on-save t)
   :hook (
-         (terraform-mode . terraform-format-on-save-mode)
+         (terraform-mode . superword-mode)
          (terraform-mode . turn-off-auto-fill)))
 
 (use-package yaml-mode
   :ensure t
   :commands yaml-mode)
+
+(use-package yaml-pro
+  :ensure t
+  :requires yaml
+  :hook (yaml-ts-mode . yaml-pro-ts-mode))
+
+(use-package indent-tools
+  :ensure t
+  :commands indent-tools-minor-mode)
 
 (use-package git-commit
   :ensure t
@@ -199,7 +214,8 @@ ring-bell-function 'ignore
                                      jde-mode
                                      lua-mode
                                      python-mode
-                                     yaml-mode))
+                                     yaml-mode
+                                     yaml-ts-mode))
   :config
   (global-company-mode 1))
 
@@ -207,13 +223,23 @@ ring-bell-function 'ignore
   :ensure t
   :requires (flycheck company)
   :init
-  (setq lsp-keymap-prefix "C-c l")
-  :hook ((rust-mode . lsp))
-  :commands lsp)
+  (setq lsp-keymap-prefix "C-c l"
+        lsp-terraform-ls-prefill-required-fields t)
+  :hook ((rust-mode . lsp-deferred)
+         (terraform-mode . lsp-deferred))
+  :commands (lsp lsp-deferred))
 
 (use-package lsp-ui
   :ensure t
   :commands lsp-ui-mode)
+
+(use-package helm-lsp
+  :ensure t
+  :commands helm-lsp-workspace-symbol)
+
+(use-package lsp-treemacs
+  :ensure t
+  :commands lsp-treemacs-errors-list)
 
 (use-package json-mode
   :ensure t
@@ -240,7 +266,9 @@ ring-bell-function 'ignore
 
 (use-package highlight-indent-guides
   :ensure t
-  :hook (yaml-mode . highlight-indent-guides-mode))
+  :hook (
+         (yaml-mode . highlight-indent-guides-mode))
+         (yaml-ts-mode . highlight-indent-guides-mode))
 
 (use-package pinentry
   :ensure t
@@ -248,7 +276,13 @@ ring-bell-function 'ignore
   (pinentry-start))
 
 (use-package vterm
-  :ensure t)
+  :ensure t
+  :configure
+  (setq vterm-buffer-name-string "vterm %s")
+  (setq vterm-max-scrollback 10000)
+  :commands vterm)
 
 (use-package typo
   :ensure t)
+
+(put 'narrow-to-page 'disabled nil)
